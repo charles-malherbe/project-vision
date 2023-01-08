bc5537fb38003cc85efdd2b7ef0da837eb97518b
charles-malherbe <Wperrie13720@!>
2023-01-08 22:53:16 +0100

    [+] Add input for directory

---
+++ README.md
@@ -0,0 +1,35 @@
+
+# Détection de logo sur image
+
+Détection de logo sur image statique avec algorithme SIFT et ORB.
+
+
+## Prérequis
+
+#### Tkinter
+
+``pip install tk``
+
+#### Pillow
+
+``pip install pillow``
+
+#### MatPlotLib
+
+``pip install matplotlib``
+
+#### OpenCV
+
+``pip install opencv-python``
+
+## Demonstration
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+## Authors
+
+- [@charles-malherbe](https://www.github.com/charles-malherbe)
+
+## Support
+
+Pour toutes questions : contact@charles-malherbe.com
--- Window.py
+++ Window.py
@@ -12,16 +12,24 @@ class Window(Tk, ):
         self.title('Identification de logo')
         self.resizable(False, False)
         self.geometry('800x800')
-        self.iconbitmap('includes/icon.ico')
         self.labelFrame = ttk.LabelFrame(self, text="Choisir une image")
+        self.input()
         self.button()
+
+    def input(self):
+        font = ('Calibri 10')
+        label = ttk.Label(self, text='Répertoire où se trouvent les logos à identifier puis sélectionner une image :', width=80, font=font)
+        label.grid(column=1, row=0, padx=10, pady=20)
+        self.entry = ttk.Entry(self, width=40)
+        self.entry.insert(0, "./images_library")
+        self.entry.grid(column=1, row=1, pady=0)
 
     def button(self):
         self.button = ttk.Button(self, text="Choisir une image", command=self.file_dialog)
-        self.button.grid(column=1, row=1, padx=350, pady=50)
+        self.button.grid(column=1, row=2, padx=350, pady=20)
 
     def file_dialog(self):
-        self.filename = filedialog.askopenfilename(initialdir="/", title="Sélectionnez une image", filetype=(
+        self.filename = filedialog.askopenfilename(initialdir="/", title="Sélectionnez une image", filetypes=(
         ("JPEG files", "*.jpg"), ("PNG files", "*.png"), ("JPG files", "*.jpg"), ("ALL files", "*.*")))
         self.show_image()
         self.recognizer = Recognizer(self.filename, self.images_path)
@@ -32,9 +40,9 @@ class Window(Tk, ):
         self.render = ImageTk.PhotoImage(self.load.resize((500, 500), Image.ANTIALIAS))
         self.img = Label(self, image=self.render)
         self.img.image = self.render
-        self.img.grid(column=1, row=2, padx=50, pady=0)
+        self.img.grid(column=1, row=3, padx=50, pady=0)
 
     def show_text(self):
         font = ('Calibri 18 bold')
         self.text = Label(self, text=self.recognizer.type, font=font)
-        self.text.grid(column=1, row=3, padx=50, pady=50)
+        self.text.grid(column=1, row=4, padx=50, pady=50)
