ad4ce412a10a6ce1fe50e296e874247a8225a77c
charles-malherbe <Wperrie13720@!>
2023-01-09 15:16:09 +0100

    [+] Add input for directory

--- Recognizer.py
+++ Recognizer.py
@@ -1,8 +1,6 @@
-import cv2
-import traceback
 import glob
-import numpy as np
-from matplotlib import pyplot as plt
+
+import cv2
 
 
 class Recognizer:
@@ -11,26 +9,44 @@ class Recognizer:
         self.library = self.charge_library(images_path)
         self.compare()
 
+    def get_bw_image(self, image):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        return image
+
     def charge_image(self, filename):
         image = cv2.imread(filename, cv2.IMREAD_COLOR)
         image = cv2.resize(image, (500, 500), interpolation=cv2.INTER_AREA)
         return image
 
     def charge_library(self, images_path):
-        path = glob.glob(images_path + "/*.png")
         files = []
-        for file in path:
+        for file in glob.glob(images_path.get() + "/*.png"):
             image = cv2.imread(file, cv2.IMREAD_COLOR)
             image = cv2.resize(image, (500, 500), interpolation=cv2.INTER_AREA)
-            name = file.split("\\")[-1].split(".")[0]
-            features = 0
-            files.append((name.capitalize(), image, features))
+            name = file.split("/")[-1].split(".")[0].capitalize()
+            files.append((name, image))
         return files
 
+    def create_detector(self):
+        detector = cv2.ORB_create()
+        return detector
+
+    def detect_logo(self, logo):
+        detector = self.create_detector()
+        matcher = self.create_matcher()
+        kp_logo, des_logo = detector.detectAndCompute(self.get_bw_image(logo), None)
+        kp_image, des_image = detector.detectAndCompute(self.get_bw_image(self.image), None)
+        matches = matcher.match(des_logo, des_image)
+        final = cv2.drawMatches(logo, kp_logo, self.image, kp_image, matches[:20], None, flags=2)
+        cv2.imshow("Comparaison", self.image)
+
+    def create_matcher(self):
+        matcher = cv2.BFMatcher()
+        return matcher
 
     def compare(self):
         self.type = "AUCUN LOGO RECONNU"
         for index in self.library:
-            name = index[0].capitalize()
-            image = index[1]
-            features = index[2]
+            name = index[0]
+            logo = index[1]
+            self.detect_logo(logo)
--- Window.py
+++ Window.py
@@ -1,36 +1,38 @@
 from tkinter import *
-from tkinter import ttk
 from tkinter import filedialog
+from tkinter import ttk
+
 from PIL import ImageTk, Image
+
 from Recognizer import *
+
 
 class Window(Tk, ):
     def __init__(self):
         super(Window, self).__init__()
-        # Répertoire des images à comparer
-        self.images_path = "images_library"
         self.title('Identification de logo')
         self.resizable(False, False)
         self.geometry('800x800')
         self.labelFrame = ttk.LabelFrame(self, text="Choisir une image")
         self.input()
-        self.button()
+        self.set_button()
 
     def input(self):
         font = ('Calibri 10')
-        label = ttk.Label(self, text='Répertoire où se trouvent les logos à identifier puis sélectionner une image :', width=80, font=font)
+        label = ttk.Label(self, text='Répertoire où se trouvent les logos à identifier puis sélectionner une image :',
+                          width=80, font=font)
         label.grid(column=1, row=0, padx=10, pady=20)
-        self.entry = ttk.Entry(self, width=40)
-        self.entry.insert(0, "./images_library")
-        self.entry.grid(column=1, row=1, pady=0)
+        self.images_path = ttk.Entry(self, width=40)
+        self.images_path.insert(0, "./images_library")
+        self.images_path.grid(column=1, row=1, pady=0)
 
-    def button(self):
+    def set_button(self):
         self.button = ttk.Button(self, text="Choisir une image", command=self.file_dialog)
         self.button.grid(column=1, row=2, padx=350, pady=20)
 
     def file_dialog(self):
-        self.filename = filedialog.askopenfilename(initialdir="/", title="Sélectionnez une image", filetypes=(
-        ("JPEG files", "*.jpg"), ("PNG files", "*.png"), ("JPG files", "*.jpg"), ("ALL files", "*.*")))
+        self.filename = filedialog.askopenfilename(initialdir="./", title="Sélectionnez une image", filetypes=(
+            ("PNG files", "*.png"), ("JPEG files", "*.jpg"), ("JPG files", "*.jpg"), ("ALL files", "*.*")))
         self.show_image()
         self.recognizer = Recognizer(self.filename, self.images_path)
         self.show_text()
